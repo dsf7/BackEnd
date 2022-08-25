3ba4481bfed36cf705426a074c71073a0b1909c6
dsf7 <89047938+dsf7@users.noreply.github.com>
2022-08-24 21:05:08 -0300

    Add files via upload

---
+++ exercicio_professor/src/main/java/com/dh/exercicio_professor/service/TrainerService.java
@@ -0,0 +1,11 @@
+package com.dh.exercicio_professor.service;
+
+import com.dh.exercicio_professor.model.Trainer;
+
+import java.util.List;
+
+public interface TrainerService {
+
+    List<Trainer> listTrainer();
+
+}
